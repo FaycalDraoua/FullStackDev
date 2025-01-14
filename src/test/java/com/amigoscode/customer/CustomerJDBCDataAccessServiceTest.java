32d4ee6a949fd56c67946333ab24105c0c343cb5
faycal <faycal.draoua.derbouz@gmail.com>
2025-01-14 11:31:47 -0500

    ajouter les test de insertCustomer() & existePersonneByEmail() & deleteCustomerByid()

--- src/test/java/com/amigoscode/customer/CustomerJDBCDataAccessServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerJDBCDataAccessServiceTest.java
@@ -2,6 +2,8 @@ package com.amigoscode.customer;
 
 import com.amigoscode.AbstractTestContainers;
 import static org.assertj.core.api.Assertions.*;
+
+import com.github.javafaker.Name;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -74,29 +76,92 @@ class CustomerJDBCDataAccessServiceTest extends AbstractTestContainers {
     @Test
     void insertCustomer() {
         //Given
+        String name = FAKER.name().firstName();
+        String email = FAKER.internet().emailAddress();
+        Customer customer = new Customer(name, email, 20);
 
         //When
+    underTest.insertCustomer(customer);
+        Optional<Customer> actual = underTest.selectAllCustomers()
+            .stream().filter(c -> c.getEmail().equals(email))
+            .findFirst();
 
         //Then
+        assertThat(actual).isPresent().hasValueSatisfying(
+                c -> {
+                    assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+                    assertThat(c.getName()).isEqualTo(customer.getName());
+                    assertThat(c.getAge()).isEqualTo(customer.getAge());
+                }
+        );
+        }
+
+    @Test
+    void willReturnEmptyWhenSelectAllCustomerById() {
+        //Given
+        String name = FAKER.name().firstName();
+        String email = FAKER.internet().emailAddress();
+        Customer customer = new Customer(name, email,30);
+
+        underTest.insertCustomer(customer);
+
+        //When
+        var actual = underTest.selectCustomerById(-1);
+
+        //Then
+        assertThat(actual).isEmpty();
     }
 
     @Test
     void existePersonWithEmail() {
         //Given
+        String name = FAKER.name().firstName();
+        String email = FAKER.internet().emailAddress();
+        Customer customer = new Customer(name,email,30);
+        underTest.insertCustomer(customer);
 
         //When
+        Optional<Customer> actual = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .findFirst();
+
+        Boolean actualEmail = underTest.existePersonWithEmail(email);
 
         //Then
+        assertThat(actual).isPresent().hasValueSatisfying(
+                c ->{
+                        assertThat(c.getName()).isEqualTo(customer.getName());
+                        assertThat(actual.get().getEmail()).isEqualTo(customer.getEmail());
+        });
+        assertThat(actualEmail).isTrue();
     }
 
     @Test
     void deleteCustomerById() {
         //Given
+        String name = FAKER.name().firstName();
+        String email = FAKER.internet().emailAddress();
+        Customer customer = new Customer(name,email,30);
+
+        underTest.insertCustomer(customer);
+
+        Integer id = underTest.selectAllCustomers()
+                .stream()
+                        .filter(c -> c.getEmail().equals(email))
+                                .map(Customer::getId)
+                                        .findFirst()
+                                                .orElseThrow();
 
         //When
+        underTest.deleteCustomerById(id);
 
         //Then
+        assertThat(underTest.selectAllCustomers()).isEmpty();
     }
+
+    @Test
+    void
 
     @Test
     void existPersonWithId() {
