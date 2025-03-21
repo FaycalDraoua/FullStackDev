694040189a725cc3f4921997d838ede853909e80
Fayçal <derbouz_draoua.fayal@courrier.uqam.ca>
2025-03-21 17:56:12 -0400

    ajouter code send a post request et get all customer dans la methode canRegisterACustomer() de la class de test CustomerIntegrationTest

--- src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
+++ src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
@@ -1,16 +1,22 @@
 package com.amigoscode.journey;
 
+import com.amigoscode.customer.Customer;
 import com.amigoscode.customer.CustomerRegistrationRequest;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CustomerIntegrationTest {
@@ -48,6 +54,26 @@ public class CustomerIntegrationTest {
                 .body(Mono.just(request), CustomerRegistrationRequest.class)
                 .exchange()
                 .expectStatus().isOk();
+
+        /// 3. get all cutomers
+        List<Customer> allCustomers = webTestClient.get().uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {})
+                .returnResult()
+                .getResponseBody();
+
+        Customer expectedCustomer = new Customer(name, email, age);
+
+        assertThat(allCustomers).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id").contains(expectedCustomer);
+
+        /// 4.make sure that customer is present
+
+
+
+        /// 5.Get customer by id
+
 
 
 
