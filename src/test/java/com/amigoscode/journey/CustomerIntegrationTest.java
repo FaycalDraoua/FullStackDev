975f8841e3336d086d46f747477d5261fe725ee2
Fayçal <derbouz_draoua.fayal@courrier.uqam.ca>
2025-03-24 18:19:55 -0400

    debut de canDeleteCustomer de la class test CustomerIntegrationTest

--- src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
+++ src/test/java/com/amigoscode/journey/CustomerIntegrationTest.java
@@ -64,16 +64,88 @@ public class CustomerIntegrationTest {
                 .returnResult()
                 .getResponseBody();
 
+        /// 4.make sure that customer is present
         Customer expectedCustomer = new Customer(name, email, age);
 
         assertThat(allCustomers).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id").contains(expectedCustomer);
 
-        /// 4.make sure that customer is present
 
+        /// 5.Get customer by id
+        var id = allCustomers.stream()
+                .filter(customer -> {
+                    return customer.getEmail().equals(email);
+                })
+            .map(Customer::getId)
+            .findFirst()
+            .orElseThrow();
+
+        expectedCustomer.setId(id);
+
+         webTestClient.get().uri("/api/v1/customers/{customerId}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>(){})
+                .isEqualTo(expectedCustomer);
+
+
+
+    }
+
+    @Test
+    void canDeleteCustomer(){
+        //Given
+        /// 1. create registration request
+        Faker faker = new Faker();
+        Name fakerNa = faker.name();
+        String name = faker.name().fullName();
+
+        String email = fakerNa.lastName()+"-"+ UUID.randomUUID() + "@foobarhello123.com";
+
+        int age = faker.number().numberBetween(18, 100);
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
+
+        //When, Then
+        /// 2. send a post request
+        webTestClient.post().uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk();
+
+        /// 3. get all cutomers
+        List<Customer> allCustomers = webTestClient.get().uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {})
+                .returnResult()
+                .getResponseBody();
+
+        /// 4.make sure that customer is present
+        Customer expectedCustomer = new Customer(name, email, age);
+
+        assertThat(allCustomers).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id").contains(expectedCustomer);
 
 
         /// 5.Get customer by id
+        var id = allCustomers.stream()
+                .filter(customer -> {
+                    return customer.getEmail().equals(email);
+                })
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
 
+        expectedCustomer.setId(id);
+
+        webTestClient.get().uri("/api/v1/customers/{customerId}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>(){})
+                .isEqualTo(expectedCustomer);
 
 
 
