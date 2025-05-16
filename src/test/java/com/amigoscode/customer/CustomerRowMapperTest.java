e501a3b6292fc5a84098e56a879f953aad7b99ae
faycal <faycal.draoua.derbouz@gmail.com>
2025-05-16 19:22:14 -0400

    juste un commit pour pour puller

--- src/test/java/com/amigoscode/customer/CustomerRowMapperTest.java
+++ src/test/java/com/amigoscode/customer/CustomerRowMapperTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class CustomerRowMapperTest {
+class   CustomerRowMapperTest {
 
     @Test
     void mapRow() throws SQLException {
