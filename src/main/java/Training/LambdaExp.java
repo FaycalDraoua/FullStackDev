a514698ac36f77739f5089b431d2d5ab5a26858a
faycal <faycal.draoua.derbouz@gmail.com>
2025-06-19 18:19:37 -0400

    remove trainning package

--- src/main/java/Training/LambdaExp.java
+++
@@ -1,5 +0,0 @@
-package Training;
-
-public interface LambdaExp {
-   void run(String name, int age);
-}
--- src/main/java/Training/MathOperat.java
+++
@@ -1,6 +0,0 @@
-package Training;
-
-public interface MathOperat {
-
-    public int operation(int a, int b);
-}
